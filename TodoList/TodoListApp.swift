e9a3658ebfdcb994897f416f33a16714553c1181
germainkevinbusiness <67168053+kevingermainbusiness@users.noreply.github.com>
2023-10-23 18:42:17 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Kevin Germain on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
